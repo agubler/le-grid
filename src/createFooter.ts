a78ff404c02e092f098580a99e421b1be010fdd5
Anthony Gubler <agubler@sitepen.com>
2017-01-20 10:22:51 +0000

    footer

--- src/createFooter.ts
+++ src/createFooter.ts
@@ -1,5 +1,5 @@
 import { VNodeProperties } from '@dojo/interfaces/vdom';
-import { Widget, WidgetMixin, WidgetProperties, WidgetFactory } from '@dojo/widgets/interfaces';
+import { Widget, WidgetMixin, WidgetProperties, WidgetFactory, DNode } from '@dojo/widgets/interfaces';
 import createWidgetBase from '@dojo/widgets/createWidgetBase';
 import registryMixin, { RegistryMixin, RegistryMixinProperties }  from '@dojo/widgets/mixins/registryMixin';
 import { v } from '@dojo/widgets/d';
@@ -15,6 +15,7 @@ export interface DgridFooterProperties extends WidgetProperties, RegistryMixinPr
 
 export interface DgridFooterMixin extends WidgetMixin<DgridFooterProperties>, RegistryMixin {
 	onClick(this: DgridFooter, evt: MouseEvent): void;
+	createPageLink(this: DgridFooter, page: string, visable: boolean, disabled: boolean): DNode;
 }
 
 export type DgridFooter = Widget<DgridFooterProperties> & DgridFooterMixin
@@ -30,8 +31,18 @@ const createDgridFooter: DgridFooterFactory = createWidgetBase
 		onClick(this: DgridFooter, evt: any) {
 			this.properties.onPaginationRequest && this.properties.onPaginationRequest(evt.target.attributes['page'].value);
 		},
+		createPageLink(this: DgridFooter, page: string, visable: boolean, disabled: boolean): DNode {
+			if (visable) {
+				const classes = {
+					'dgrid-page-disabled': disabled
+				};
+				return v('span.dgrid-page-link', { onclick: this.onClick, page, classes }, [ page ]);
+			}
+			return null;
+		},
 		getChildrenNodes(this: DgridFooter): VNodeProperties {
-			const { properties: { totalCount = '?', pagination, paginationDetails: { dataRangeStart = 0, dataRangeCount = 0, pageNumber = 1 } = {} } } = this;
+			const { properties: { totalCount, pagination, paginationDetails: { dataRangeStart = 0, dataRangeCount = 0, pageNumber = 1 } = {} } } = this;
+			const totalPages = Math.ceil(totalCount / dataRangeCount);
 
 			return [
 				pagination ? v('div.dgrid-pagination', [
@@ -40,15 +51,18 @@ const createDgridFooter: DgridFooterFactory = createWidgetBase
 						v('span.dgrid-previous.dgrid-page-link', { onclick: this.onClick, page: String(pageNumber - 1), classes: { 'dgrid-page-disabled': (pageNumber === 1) } }, [ '<' ]),
 						v('span.dgrid-pagination-links', [
 							v('span.dgrid-page-link', { onclick: this.onClick, page: '1', classes: { 'dgrid-page-disabled': pageNumber === 1 } }, [ '1' ]),
-							v('span.dgrid-page-skip', [ '...' ]),
-							v('span.dgrid-page-link', { onclick: this.onClick, page: String(pageNumber + 1), classes: { 'dgrid-page-disabled': 1 !== 1 } }, [ String(pageNumber + 1) ]),
-							v('span.dgrid-page-link', { onclick: this.onClick, page: String(pageNumber + 2), classes: { 'dgrid-page-disabled': 1 !== 1 } }, [ String(pageNumber + 2) ]),
-							v('span.dgrid-page-skip', [ '...' ]),
-							v('span.dgrid-page-link', { onclick: this.onClick, page: '4', classes: { 'dgrid-page-disabled': 1 === 1 } }, [ '4' ])
+							pageNumber > 3 ? v('span.dgrid-page-skip', [ '...' ]) : null,
+							this.createPageLink(String(pageNumber - 2), Boolean(pageNumber - 2 > 1), false),
+							this.createPageLink(String(pageNumber - 1), Boolean(pageNumber - 1 > 1), false),
+							this.createPageLink(String(pageNumber), Boolean(pageNumber !== 1 && pageNumber !== totalPages), true),
+							this.createPageLink(String(pageNumber + 1), Boolean(pageNumber + 1 < totalPages), false),
+							this.createPageLink(String(pageNumber + 2), Boolean(pageNumber + 2 < totalPages), false),
+							pageNumber !== totalPages ? v('span.dgrid-page-skip', [ '...' ]) : null,
+							v('span.dgrid-page-link', { onclick: this.onClick, page: String(totalPages), classes: { 'dgrid-page-disabled': pageNumber === totalPages } }, [ String(totalPages) ])
 						]),
-						v('span.dgrid-next.dgrid-page-link', { onclick: this.onClick, page: String(pageNumber + 1), classes: { 'dgrid-page-disabled': ((1 * 10) >= 100) } }, [ '>' ])
+						v('span.dgrid-next.dgrid-page-link', { onclick: this.onClick, page: String(pageNumber + 1), classes: { 'dgrid-page-disabled': ((pageNumber * dataRangeCount) >= totalCount) } }, [ '>' ])
 					])
-				]) : null
+				]) : v('div.dgrid-status', [ `${totalCount} results` ]),
 			];
 		}
 	}
