a017ef8685c475c82ec043eb7df6436630c582fe
Anthony Gubler <agubler@sitepen.com>
2017-01-21 20:03:13 +0000

    initial tests structure

---
+++ tests/intern-saucelabs.ts
@@ -0,0 +1,12 @@
+export * from './intern';
+
+export const environments = [
+	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
+	{ browserName: 'MicrosoftEdge', platform: 'Windows 10' },
+	{ browserName: 'chrome', platform: 'Windows 10' }
+];
+
+/* SauceLabs supports more max concurrency */
+export const maxConcurrency = 4;
+
+export const tunnel = 'SauceLabsTunnel';
