3ed4579a8f3326cbb266fb11e20d9a64f02ecac0
Anthony Gubler <agubler@sitepen.com>
2017-01-31 01:01:55 +0000

    loader config

--- tests/functional/index.ts
+++ tests/functional/index.ts
@@ -4,7 +4,8 @@
 		{ name: 'src', location: '_build/src' },
 		{ name: 'tests', location: '_build/tests' },
 		{ name: '@dojo', location: 'node_modules/@dojo' },
-		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' }
+		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' },
+		{ name: 'immutable', location: 'node_modules/immutable/dist', main: 'immutable' }
 	]
 });
 
