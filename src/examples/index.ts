2501dc0da188900d01aa02057563c714c4e36ac6
Anthony Gubler <agubler@sitepen.com>
2017-03-19 20:30:44 +0000

    update loader configuration

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -3,6 +3,7 @@
 	packages: [
 		{ name: '@dojo', location: '../../../node_modules/@dojo' },
 		{ name: 'maquette', location: '../../../node_modules/maquette/dist', main: 'maquette' },
+		{ name: 'pepjs', location: '../../../node_modules/pepjs/dist', main: 'pep' },
 		{ name: 'immutable', location: '../../../node_modules/immutable/dist', main: 'immutable' }
 	]
 });
