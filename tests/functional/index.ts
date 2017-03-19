4424af207804db066ea165cfedea4af9eb4c65af
Anthony Gubler <agubler@sitepen.com>
2017-03-19 20:44:01 +0000

    missing functional test config

--- tests/functional/index.ts
+++ tests/functional/index.ts
@@ -4,6 +4,7 @@
 		{ name: 'src', location: '_build/src' },
 		{ name: 'tests', location: '_build/tests' },
 		{ name: '@dojo', location: 'node_modules/@dojo' },
+		{ name: 'pepjs', location: 'node_modules/pepjs/dist', main: 'pep' },
 		{ name: 'maquette', location: 'node_modules/maquette/dist', main: 'maquette' },
 		{ name: 'immutable', location: 'node_modules/immutable/dist', main: 'immutable' }
 	]
