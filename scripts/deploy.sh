16bcf5e95acd1918f656112c1435613ac7183013
Anthony Gubler <agubler@sitepen.com>
2018-06-01 12:36:28 +0100

    add remote

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,6 @@ npm run examples
 git add -f examples/output/dist
 git commit -am "build examples"
 git filter-branch -f --prune-empty --subdirectory-filter examples/output/dist
+git remote add ssh-remote git@github.com:agubler/le-grid.git
 git push -f ssh-remote gh-pages
 git checkout -
