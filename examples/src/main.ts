f12fe94527c063a628d61c8b0ea4ab6ea66adebc
Anthony Gubler <agubler@sitepen.com>
2018-07-04 15:12:34 +0100

    Add restful example

--- examples/src/main.ts
+++ examples/src/main.ts
@@ -5,6 +5,7 @@ import Grid from 'le-grid';
 import { createFetcher, createUpdater } from 'le-grid/utils';
 
 import { createData } from './data';
+import { FetcherOptions } from 'le-grid/interfaces';
 
 const columnConfig = [
 	{
@@ -35,9 +36,46 @@ const data = createData(200000);
 const fetcher = createFetcher(data);
 const updater = createUpdater(data);
 
+const restfulFetcher = async (page: number, pageSize: number, options: FetcherOptions = {}) => {
+	const offset = (page - 1) * pageSize;
+	const response = await fetch(`https://mock-json-server.now.sh/data?offset=${offset}&size=${pageSize}`, {
+		method: 'POST',
+		body: JSON.stringify({
+			sort: options.sort,
+			filter: options.filter,
+			offset,
+			size: pageSize
+		}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	const json = await response.json();
+	return { data: json.data, meta: { total: json.total } };
+};
+
+const restfulUpdater = async (item: any) => {
+	await fetch(`https://mock-json-server.now.sh/data/${item.id}`, {
+		method: 'POST',
+		body: JSON.stringify(item),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+};
+
 class App extends WidgetBase {
 	render() {
-		return v('div', { styles: { height: '400px' } }, [w(Grid, { columnConfig, fetcher, updater })]);
+		return v('div', { styles: { display: 'flex', justifyContent: 'space-around' } }, [
+			v('div', { key: 'local', styles: { height: '400px' } }, [
+				v('h1', ['Local Data']),
+				w(Grid, { columnConfig, fetcher, updater })
+			]),
+			v('div', { key: 'rest', styles: { height: '400px' } }, [
+				v('h1', ['RESTFUL API Data']),
+				w(Grid, { columnConfig, fetcher: restfulFetcher, updater: restfulUpdater })
+			])
+		]);
 	}
 }
 
