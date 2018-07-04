e73d8d74a824501f005178bfb3e46c611d913aac
Anthony Gubler <agubler@sitepen.com>
2018-07-04 21:56:09 +0100

    Revert "Remove ssh from deployment"
    This reverts commit 05519c5d07af8befbf87c3be49bc9d26b12352a4.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/XXXXX)"
+openssl aes-256-cbc -K $encrypted_ca9b4100768e_key -iv $encrypted_ca9b4100768e_iv -in "github_deploy_key.enc" -out "$SSH_FILE" -d
+chmod 600 "$SSH_FILE" && printf "%s\n" "Host github.com"  "  IdentityFile $SSH_FILE" "  LogLevel ERROR" >> ~/.ssh/config
 npm install
 git checkout -B gh-pages
 npm run examples
