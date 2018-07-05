f0e1122cd8030042d7531daf3b70e35932c4ddcf
Anthony Gubler <agubler@sitepen.com>
2018-07-05 10:12:03 +0100

    Do not use faker in the client

--- examples/src/data.ts
+++ examples/src/data.ts
@@ -1,16 +1,29 @@
-import * as faker from 'faker';
+const names = ['Bill', 'Bess', 'Zara', 'Sarah', 'Matt', 'Antony', 'Tom', 'Eve', 'Flo', 'Gill'];
+const surnames = [
+	'Langworth',
+	'Shanahan',
+	'Connelly',
+	'Sauer',
+	'Von',
+	'Willms',
+	'Zulauf',
+	'Mann',
+	'Gutkowski',
+	'Ernser'
+];
+const titles = ['Mr.', 'Miss', 'Ms.', 'Mrs.', 'Dr.'];
 
 export function createData(size: number = 1000) {
 	const data: any[] = [];
 	for (let i = 0; i < size; i++) {
+		const nameIndex = Math.floor(Math.random() * 10);
+		const surnameIndex = Math.floor(Math.random() * 10);
+		const titleIndex = Math.floor(Math.random() * 5);
 		data.push({
 			id: i,
-			title: faker.name.prefix(),
-			firstName: faker.name.firstName(),
-			lastName: faker.name.lastName(),
-			email: faker.internet.email(),
-			street: faker.address.streetAddress(),
-			zipCode: faker.address.zipCode()
+			title: titles[Math.floor(nameIndex / 2)],
+			firstName: names[surnameIndex],
+			lastName: surnames[titleIndex]
 		});
 	}
 	return data;
